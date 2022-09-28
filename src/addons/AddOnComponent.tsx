1a3e04c33bcdefeddafe46788e78623221c583de
Alexey Smirnov <alexey.smirnov@wework.com>
2022-09-28 19:05:33 +0300

    add quantity

--- src/addons/AddOnComponent.tsx
+++ src/addons/AddOnComponent.tsx
@@ -1,4 +1,4 @@
-import { AddOnView } from './addOnsTypes';
+import { AddOnView } from "./addOnsTypes";
 
 type AddOnComponentProps = AddOnView;
 
@@ -11,12 +11,17 @@ const AddOnComponent = (props: AddOnComponentProps) => {
       <br />
       <label>
         select
-        <input
-          onChange={props.select}
-          checked={props.isSelected}
-          type="checkbox"
-        />
+        <input onChange={props.select} checked={props.isSelected} type="checkbox" />
       </label>
+      <label htmlFor="quantity">quantity</label>
+      <button onClick={() => props.setQuantity(props.quantity - 1)}>-</button>
+      <input
+        id="quantity"
+        value={props.quantity}
+        type="number"
+        onChange={(e) => props.setQuantity(+e.target.value)}
+      />
+      <button onClick={() => props.setQuantity(props.quantity + 1)}>+</button>
     </div>
   );
 };
--- src/addons/addOnsApi.ts
+++ src/addons/addOnsApi.ts
@@ -3,7 +3,7 @@ import { AddOn, AddOnPayload, AddOnView } from './addOnsTypes';
 export const papiGetAddonRequest = (...args: any): AddOn[] => {
   return [
     { uuid: "uuid-1", name: "Super cool addon", price: "10$" },
-    { uuid: "uuid-1", name: "Even better addon", price: "20$" },
+    { uuid: "uuid-2", name: "Even better addon", price: "20$" },
     { uuid: "uuid-nina", name: "Ninyn borsch", price: "priceless" },
   ];
 };
