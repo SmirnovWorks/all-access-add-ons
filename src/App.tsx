bfa446832b887126062f68874da58833c02d0751
Alexey Smirnov <93927092+SmirnovWorks@users.noreply.github.com>
2022-09-28 15:54:47 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+interface AddOn {
+  uuid: string;
+  name: string;
+  price: string;
+}
+
+interface AddOnView extends AddOn {
+  isSelected: boolean;
+  quantity: number;
+  select(): void;
+  setQuantity(quantity: number): void;
+}
+
+interface AddOnPayload extends AddOn {
+  uuid: string;
+  quantity: number;
+}
+
+const papiGetAddonRequest = (...args: any): AddOn[] => {
+  return [
+    { uuid: "1", name: "addon-1", price: "10$" },
+    { uuid: "2", name: "addon-2", price: "20$" },
+    { uuid: "nina", name: "Ninin borsch", price: "priceless" }
+  ];
+};
+
+const papiPostSubtotal = (args: AddOnPayload[]) => {
+  alert("SUBMIT" + JSON.stringify(args, null, 2));
+};
+
+const useAddonView = (addon: AddOn): AddOnView => {
+  const [storage, setStorage] = useState({
+    ...addon,
+    quantity: 1,
+    isSelected: false
+  });
+
+  const select = () =>
+    setStorage((addon) => ({ ...addon, isSelected: !addon.isSelected }));
+
+  const setQuantity = (quantity: number) =>
+    setStorage((addon) => ({ ...addon, quantity }));
+
+  return {
+    ...storage,
+    select,
+    setQuantity
+  };
+};
+
+const useAddons = (
+  locationUuid: string,
+  productType: string
+): [AddOnView[], AddOnPayload[], AddOn[]] => {
+  const addons: AddOn[] = papiGetAddonRequest(locationUuid, productType);
+
+  const addonsView: AddOnView[] = addons.map(useAddonView);
+
+  const selectedAddons: AddOnPayload[] = addonsView.filter(
+    (addon) => addon.isSelected
+  );
+
+  return [addonsView, selectedAddons, addons];
+};
+
+export default function App() {
+  const [addonsView, selectedAddons] = useAddons("location", "all_access");
+
+  const handleSubmit = () => {
+    papiPostSubtotal(selectedAddons);
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {addonsView.map((addon) => {
+        return (
+          <div style={{ border: "1px solid black" }}>
+            Addon: {addon.name}
+            <br />
+            Price: {addon.price}
+            <br />
+            <label>
+              select
+              <input onChange={addon.select} type="checkbox" />
+            </label>
+          </div>
+        );
+      })}
+      <hr />
+      {selectedAddons.map((selectedAddon) => (
+        <b>{selectedAddon.name}, </b>
+      ))}
+      <br />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
